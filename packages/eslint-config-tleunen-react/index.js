c50fccc75dede51fcab6ee5c30f3312b2fe9eba5
Tommy Leunen <tommy.leunen+github@gmail.com>
2016-12-23 21:20:12 -0500

    fix: Fix extend path

--- packages/eslint-config-tleunen-react/index.js
+++ packages/eslint-config-tleunen-react/index.js
@@ -1,7 +1,7 @@
 module.exports = {
     extends: [
         'eslint-config-airbnb',
-        '../eslint-config-tleunen/rules/override',
+        'eslint-config-tleunen/rules/override',
     ].map(require.resolve),
     env: {
         browser: true,
