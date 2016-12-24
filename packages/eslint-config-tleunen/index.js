94e63c13fb3c4daa3277666b610341404df55517
Tommy Leunen <tommy.leunen+github@gmail.com>
2016-12-23 21:15:10 -0500

    refactor: Move rules

--- packages/eslint-config-tleunen/index.js
+++ packages/eslint-config-tleunen/index.js
@@ -3,17 +3,9 @@
 module.exports = {
     extends: [
         'eslint-config-airbnb-base',
+        './rules/override',
     ].map(require.resolve),
     parserOptions: {
         sourceType: 'script',
-    },
-    rules: {
-        indent: ['error', 4, { SwitchCase: 1, VariableDeclarator: 1, outerIIFEBody: 1 }],
-        'max-len': ['error', 120, 4, {
-            ignoreUrls: true,
-            ignoreComments: false,
-            ignoreStrings: true,
-            ignoreTemplateLiterals: true,
-        }],
     },
 };
